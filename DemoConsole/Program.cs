49281cc29f6f265788e1fe402cb8652b5c9b2952
ITPROVN86 <quangltn3@fe.edu.vn>
2023-10-07 20:30:30 +0700

    Add project files.

---
+++ DemoConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*var member = new Member() { Id = 1, Name = "An" };
+            Console.WriteLine("Id: "+ member.Id+ ", Name: " + member.Name);*/
+            new Bai17().Run();
+        }
+    }
+
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
