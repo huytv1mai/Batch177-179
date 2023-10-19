2d602a3b6b63e5c27fa26d2f515ecaee8027596e
ITPROVN86 <quangltn3@fe.edu.vn>
2023-10-19 18:45:33 +0700

    sesion 9

--- DemoConsole/Program.cs
+++ DemoConsole/Program.cs
@@ -1,21 +1,26 @@
-﻿using DemoConsole.ClassAndMethod;
-using DemoConsole.Operators;
-using DemoConsole.Session6;
-using DemoConsole.Session8;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DemoConsole.Session10;
 
+using ABC = Automotive;
 namespace DemoConsole
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            new SalaryDetail().Run();
+            new ExampleMulticaseDelegate().Run();
         }
     }
 
 }
+
+namespace Automotive
+{
+    public class SpareParts { 
+        string _spareName; 
+        public SpareParts() { _spareName = "Gear Box"; } 
+        public void Display() { 
+            Console.WriteLine("Spare Part name: " + _spareName); 
+        } 
+    }
+}
+
